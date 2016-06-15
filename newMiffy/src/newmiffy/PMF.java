ab2ba551ebf236789db651bf5fcf78574f3a0f40
a-takano <g14923ks@gm.tsuda.ac.jp>
2016-06-15 12:44:41 +0900

    できぬ

---
+++ newMiffy/src/newmiffy/PMF.java
@@ -0,0 +1,15 @@
+package newmiffy;
+
+import javax.jdo.*;
+
+//PersistenceManagerFactoryインスタンスを取得する
+public class PMF {
+    private static final javax.jdo.PersistenceManagerFactory factory = 
+        JDOHelper.getPersistenceManagerFactory("transactions-optional");
+     
+    private PMF(){}
+     
+    public static javax.jdo.PersistenceManagerFactory get(){
+        return factory;
+    }
+}
